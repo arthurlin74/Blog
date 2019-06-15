85acd08d079b8a894e488701e8d4db9c4b307081
YaHsuLin <arthurlin74@gmail.com>
2019-06-15 16:33:13 +0800

    新增部分功能

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from article.models import Article, Comment
+
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content','pubDateTime'] #pubdatetime 講義沒寫
+    list_display_links = ['article'] #也可不加
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']#建議不要加，任意修改太危險
+ 
+    class Meta:
+        model = Comment
+
+admin.site.register(Article)
+admin.site.register(Comment, CommentModelAdmin)
--- blog/article/models.py
+++ blog/article/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=128, unique=True) #128 = 字數，與bit樹無關，unique 項目唯一性，在此因文章標題為唯一性，故為 True
+    content = models.TextField()
+    pubDateTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title #回去看工研院講義第五章
+    class Meta:
+        ordering = ['-pubDateTime']
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # on_delete=models.CASCADE 表示「連串刪除」，亦即當所指的外來資料刪除時，本資料一併刪除。 所以，當某篇文章刪除時，Django 會將該文章的所屬的所有留言一併刪除
+    content = models.CharField(max_length=128)
+    pubDateTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.article.title + '-' + str(self.id)
