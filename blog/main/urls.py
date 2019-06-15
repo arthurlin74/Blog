990f9920c582575da68025aee3cb6ada33cd047e
YaHsuLin <arthurlin74@gmail.com>
2019-06-15 12:08:41 +0800

    1st

---
+++ blog/main/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from main import views
+
+
+app_name = 'main'
+<<<<<<< HEAD
+
+urlpatterns = [
+    path('',views.main, name='main'),
+    path('about/',views.about,name='about')
+]
+
+=======
+urlpatterns = [
+    path('', views.main, name='main'),
+]
+>>>>>>> branch 'master' of https://github.com/arthurlin74/blog
---
+++ blog/main/views.py
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+﻿from django.shortcuts import render
+
+# Create your views here.
+# from django.http import HttpResponse
+
+
+def main(request):
+    '''
+    Show 'Hello world!' in the main page
+    '''
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
+=======
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def main(request):
+    '''
+    Show 'Hello world!' in the main page
+    '''
+    return HttpResponse('Hello world!')
+>>>>>>> branch 'master' of https://github.com/arthurlin74/blog
