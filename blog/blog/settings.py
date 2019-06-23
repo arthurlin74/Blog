a1672792aff64d1662652fdc3847f183bd8f37ed
YaHsuLin <arthurlin74@gmail.com>
2019-06-23 15:41:34 +0800

    debug

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ura+j^3%qz*_mcn&qo!)8-87kv!-m&+n0lrx!l*-c%!+!8t-g7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if 'DYNO' in os.environ:    # Running on Heroku
     DEBUG = False
